4571eb2d944b10c8787450032b96051fd883f2b9
Hashnimo <hashithanimantha@gmail.com>
2024-04-26 14:40:20 +0530

    Update run.sh

--- scripts/run.sh
+++ scripts/run.sh
@@ -5,7 +5,9 @@ name=dynamicrafter_512_seed${seed}
 ckpt=/kaggle/working/DynamiCrafter/checkpoints/dynamicrafter_512_v1/model.ckpt
 config=/kaggle/working/DynamiCrafter/configs/inference_512_v1.0.yaml
 
-prompt_dir=$input
+input_name=$(basename /kaggle/input/*)
+
+prompt_dir=/kaggle/input/$input_name
 res_dir=/kaggle/working/DynamiCrafter/results
 
 H=576
